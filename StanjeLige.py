72038fb56ab681ac9b6caafaa76f037ceb6322cc
Klemen Kenda <klemen.kenda@ijs.si>
2022-10-28 13:36:00 +0200

    Fixing final filename

--- StanjeLige.py
+++ StanjeLige.py
@@ -46,7 +46,7 @@ for st_lige in range(1,21):
         vCsv(stanjeLige,st_tekem, score)
 
 if path.isfile('./ResnaStanja/StanjeLige'+str(st_tekem)+'.csv'):
-    g=open('ZL_2020_2021.csv','w+',encoding='utf-8')
+    g=open('ZL_2022_2023.csv','w+',encoding='utf-8')
     with open('./ResnaStanja/StanjeLige'+str(st_tekem)+'.csv','r+',encoding='utf-8') as f:
         for i in f.readlines():
             g.write(i)
